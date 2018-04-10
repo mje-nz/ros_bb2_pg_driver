a698d4f769f3d995eca8759436c33960f77675f2
Matthew Edwards <mje-nz@users.noreply.github.com>
2018-04-11 11:27:26 +1200

    Publish camera info, fix colour order

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,27 @@
  * Author: Alexandre Coninx (ISIR CNRS/UPMC), 17/02/2015
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <ctime>
+#include <cstdint>
+#include <cstdio>
 
-#include <sys/stat.h>
-#include <errno.h>
-
-#include <sstream>
-
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 
 #include "rectify_image.hpp"
 
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "sensor_msgs/Image.h"
-#include <image_transport/image_transport.h>
 
 
 // Inspired from https://github.com/ros-perception/vision_opencv/blob/indigo/cv_bridge/src/cv_bridge.cpp @347-367
-void triclopscolorimage_to_imagemsg(const TriclopsColorImage &tci, sensor_msgs::Image &im, unsigned long int id,
-                                    ros::Time t) {
-    im.header.stamp = t;
+void triclopscolorimage_to_imagemsg(const TriclopsColorImage &tci, sensor_msgs::Image &im, unsigned int id) {
     im.header.seq = id;
-    im.height = tci.nrows;
-    im.width = tci.ncols;
-    im.encoding = std::string("rgba8");
+    im.height = static_cast<uint32_t>(tci.nrows);
+    im.width = static_cast<uint32_t>(tci.ncols);
+    im.encoding = std::string("bgra8");
     im.is_bigendian = 0;
-    im.step = tci.rowinc;
-    size_t size = im.step * im.height;
+    im.step = static_cast<uint32_t>(tci.rowinc);
+    size_t size = im.step*im.height;
     im.data.resize(size);
     memcpy((char *) (&im.data[0]), &tci.data[0], size);
 }
@@ -52,16 +43,23 @@ public:
         configureCamera(camera_);
         generateTriclopsContext(camera_, context_);
 
+        // Fill in camera info
         float f, cx, cy, baseline;
+        camera_info_left_.width = 1024;  // Hardcoded for now
+        camera_info_left_.height = 768;
         triclopsGetFocalLength(context_, &f);
+        camera_info_left_.P[0] = f;
+        camera_info_left_.P[5] = f;
         triclopsGetImageCenter(context_, &cy, &cx);
-        //TODO: I reckon the correct principal point is (cx*width, cy*height)
-        // My calibration has f=1326.9, cx=503.7, cy=383.2
-        printf("Stereo parameters: f=%f cx=%f cy=%f\n", f, cx, cy);
+        camera_info_left_.P[2] = cx*camera_info_left_.width;
+        camera_info_left_.P[6] = cy*camera_info_left_.height;
+        camera_info_left_.P[10] = 1;
+        camera_info_right_ = camera_info_left_;
+        triclopsGetBaseline(context_, &baseline);
+        camera_info_right_.P[3] = -f*baseline;
 
-
-        pub_left_ = it_.advertise("camera/left/image_rect_color", 10);
-        pub_right_ = it_.advertise("camera/right/image_rect_color", 10);
+        pub_left_ = it_.advertiseCamera("camera/left/image_rect_color", 10);
+        pub_right_ = it_.advertiseCamera("camera/right/image_rect_color", 10);
     }
 
     void run() {
@@ -108,12 +106,14 @@ private:
         ros::Time t = ros::Time::now();
         sensor_msgs::Image left_message, right_message;
         if (pub_left_.getNumSubscribers() > 0) {
-            triclopscolorimage_to_imagemsg(left_image, left_message, frame_index_, t);
-            pub_left_.publish(left_message);
+            triclopscolorimage_to_imagemsg(left_image, left_message, frame_index_);
+            camera_info_left_.header.seq = frame_index_;
+            pub_left_.publish(left_message, camera_info_left_, t);
         }
         if (pub_right_.getNumSubscribers() > 0) {
-            triclopscolorimage_to_imagemsg(right_image, right_message, frame_index_, t);
-            pub_right_.publish(right_message);
+            triclopscolorimage_to_imagemsg(right_image, right_message, frame_index_);
+            camera_info_right_.header.seq = frame_index_;
+            pub_right_.publish(right_message, camera_info_right_, t);
         }
 
     }
@@ -121,11 +121,12 @@ private:
     ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
 
+    image_transport::CameraPublisher pub_left_, pub_right_;
+    sensor_msgs::CameraInfo camera_info_left_, camera_info_right_;
+
     FC2::Camera camera_;
-    TriclopsContext context_;
-    unsigned long int frame_index_;
-    image_transport::Publisher pub_left_;
-    image_transport::Publisher pub_right_;
+    TriclopsContext context_ = nullptr;
+    unsigned int frame_index_ = 0;
 };
 
 
